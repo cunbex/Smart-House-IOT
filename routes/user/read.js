d91024d1d48bf0ba8e895a415990a6efe2bd8833
Cunbex <hamzalagab.tech@gmail.com>
2024-02-22 12:51:34 +0100

    Add read file route

--- routes/user/read.js
+++ routes/user/read.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('node:path');
 
 const router = express.Router();
 
@@ -7,6 +8,31 @@ router.get('/', async (req, res) => {
     try {
         const allUsers = await req.prisma.user.findMany();
         res.status(200).json(allUsers);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await req.prisma.$disconnect();
+    }
+});
+
+router.get('/picture', async (req, res) => {
+    try {
+        const result = await req.prisma.user.findUnique({
+            where: {
+                id: req.body.id,
+            },
+        });
+        const filePath = path.resolve(__dirname, `../../${result.picturePath}`);
+
+        res.status(200).sendFile(filePath, (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+                console.log('Sent:', result.picturePath);
+            }
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
