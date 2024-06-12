e1c8ca2fa60df4270bead874d8c45fdb62662d2b
cunbex <hamzalagab.tech@gmail.com>
2024-06-12 14:07:52 +0100

    Update WS

--- public/javascripts/device.js
+++ public/javascripts/device.js
@@ -55,7 +55,7 @@ function addTableRow(
     tableBody.appendChild(newRow);
 }
 
-const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket('ws://smart-house-iot.onrender.com');
 
 ws.onopen = () => {
     console.log('Connected to WebSocket server');
